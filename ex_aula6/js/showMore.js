9583dfc1caa38d21bd97922ef263bed3ee645326
Beto <jrcampos82@gmail.com>
2024-03-14 22:18:23 -0400

    event dropdown

--- ex_aula6/js/showMore.js
+++ ex_aula6/js/showMore.js
@@ -1,5 +1,19 @@
 let open = document.querySelector('article')
+let mask = document.querySelector('.mask')
+let btn = document.querySelector('button')
+
 
 function showMore() {
+
     open.classList.toggle('open')
+    if(open.classList.contains('open')) {
+        // remove a class mask
+        mask.classList.remove('mask')
+        btn.innerHTML = "Mostrar menos";
+    } else {
+        btn.innerHTML = 'Mostrar mais'
+        mask.classList.add('mask')
+    }
 }
+
+// setTimeout(showMore, 3000) // 3 segundos
---
+++ ex_aula7/js/script.js
@@ -0,0 +1,5 @@
+let menu = document.getElementsByClassName("menu");
+
+function show() {
+  menu[0].classList.toggle("hide");
+}
